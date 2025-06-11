5c1495ce564bf4dd6b4cdeaa802cb4d99e4e4e2b
zielisgabriel <josegabrielas0586@gmail.com>
2025-06-11 00:42:36 -0300

    Spelling correction

---
+++ src/main/java/br/com/gabriel/barbershop_appointment_api/BarbershopAppointmentApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.gabriel.barbershop_appointment_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BarbershopAppointmentApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BarbershopAppointmentApiApplication.class, args);
+	}
+
+}
