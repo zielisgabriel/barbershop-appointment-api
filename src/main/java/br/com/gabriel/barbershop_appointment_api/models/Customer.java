574312d099cb19199ba1bce38766661f6ebb342c
zielisgabriel <josegabrielas0586@gmail.com>
2025-06-13 11:18:32 -0300

    feat: JPA Config added to database

--- src/main/java/br/com/gabriel/barbershop_appointment_api/models/Customer.java
+++ src/main/java/br/com/gabriel/barbershop_appointment_api/models/Customer.java
@@ -2,6 +2,7 @@ package br.com.gabriel.barbershop_appointment_api.models;
 
 import java.util.UUID;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -15,8 +16,15 @@ import lombok.Data;
 public class Customer {
     @Id
     @GeneratedValue(strategy = GenerationType.UUID)
-    private UUID customer_id;
-    private String customer_name;
-    private String customer_email;
-    private String customer_password;
+    @Column(name = "customer_id")
+    private UUID customerId;
+
+    @Column(name = "customer_name", nullable = false)
+    private String customerName;
+
+    @Column(name = "customer_email", nullable = false, unique = true)
+    private String customerEmail;
+
+    @Column(name = "customer_password", nullable = false)
+    private String customerPassword;
 }
