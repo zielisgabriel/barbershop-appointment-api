821e2aebbb30c7e04a8d9b9fe88f5ef59d503ef5
zielisgabriel <josegabrielas0586@gmail.com>
2025-06-13 11:22:41 -0300

    feat: Customer Mapper added to trasform from Customer DTO to Customer Entity

--- src/main/java/br/com/gabriel/barbershop_appointment_api/mappers/CustomerMapper.java
+++ src/main/java/br/com/gabriel/barbershop_appointment_api/mappers/CustomerMapper.java
@@ -1,15 +1,18 @@
 package br.com.gabriel.barbershop_appointment_api.mappers;
 
+import org.springframework.stereotype.Component;
+
 import br.com.gabriel.barbershop_appointment_api.dtos.CustomerDTO;
 import br.com.gabriel.barbershop_appointment_api.models.Customer;
 
+@Component
 public class CustomerMapper {
     public Customer map(CustomerDTO customerDTO) {
         Customer customer = new Customer();
 
-        customer.setCustomer_name(customerDTO.getCustomer_name());
-        customer.setCustomer_email(customerDTO.getCustomer_email());
-        customer.setCustomer_password(customerDTO.getCustomer_password());
+        customer.setCustomerName(customerDTO.getCustomerName());
+        customer.setCustomerEmail(customerDTO.getCustomerEmail());
+        customer.setCustomerPassword(customerDTO.getCustomerPassword());
 
         return customer;
     }
