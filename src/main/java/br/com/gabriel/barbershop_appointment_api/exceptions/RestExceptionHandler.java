69dd0f02095d63f54b832436b22c238b285deab5
zielisgabriel <josegabrielas0586@gmail.com>
2025-06-13 11:03:10 -0300

    feat: Error Handling added

---
+++ src/main/java/br/com/gabriel/barbershop_appointment_api/exceptions/RestExceptionHandler.java
@@ -0,0 +1,32 @@
+package br.com.gabriel.barbershop_appointment_api.exceptions;
+
+import java.time.LocalDateTime;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+@ControllerAdvice
+public class RestExceptionHandler extends ResponseEntityExceptionHandler {
+    @ExceptionHandler(UserAlreadyExistsException.class)
+    private ResponseEntity<String> userNotFoundHandler(UserAlreadyExistsException error) {
+        LocalDateTime currentDateTime = LocalDateTime.now();
+        String responseJson = String.format("{\"timestamp\": \"%s\", \"message\": \"%s\"}",
+            currentDateTime,
+            error.getMessage());
+
+        return new ResponseEntity<String>(responseJson, HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(UserNotFoundException.class)
+    private ResponseEntity<String> userNotFoundHandler(UserNotFoundException error) {
+        LocalDateTime currentDateTime = LocalDateTime.now();
+        String responseJson = String.format("{\"timestamp\": \"%s\", \"message\": \"%s\"}",
+            currentDateTime,
+            error.getMessage());
+
+        return new ResponseEntity<String>(responseJson, HttpStatus.NOT_FOUND);
+    }
+}
