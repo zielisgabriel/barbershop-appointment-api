8ceb77ca1e5e1138675ad993821a990d6bfe9efb
zielisgabriel <josegabrielas0586@gmail.com>
2025-06-13 10:41:34 -0300

    refactor: Rename repository method

--- src/main/java/br/com/gabriel/barbershop_appointment_api/services/CreateCustomerService.java
+++ src/main/java/br/com/gabriel/barbershop_appointment_api/services/CreateCustomerService.java
@@ -3,6 +3,7 @@ package br.com.gabriel.barbershop_appointment_api.services;
 import org.springframework.stereotype.Service;
 
 import br.com.gabriel.barbershop_appointment_api.dtos.CustomerDTO;
+import br.com.gabriel.barbershop_appointment_api.exceptions.UserAlreadyExistsException;
 import br.com.gabriel.barbershop_appointment_api.mappers.CustomerMapper;
 import br.com.gabriel.barbershop_appointment_api.models.Customer;
 import br.com.gabriel.barbershop_appointment_api.repositories.CustomerRepository;
@@ -14,9 +15,9 @@ public class CreateCustomerService {
     private final CustomerRepository customerRepository;
     private final CustomerMapper customerMapper;
 
-    public void execute(CustomerDTO customerDTO) {
-        customerRepository.findByEmail(customerDTO.getCustomer_email()).ifPresent(customer -> {
-            throw new RuntimeException("Customer already exists");
+    public void execute(CustomerDTO customerDTO) throws UserAlreadyExistsException {
+        customerRepository.findByCustomerEmail(customerDTO.getCustomerEmail()).ifPresent(customer -> {
+            throw new UserAlreadyExistsException();
         });
 
         Customer customer = customerMapper.map(customerDTO);
