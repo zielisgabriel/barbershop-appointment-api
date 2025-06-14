973f6d09115c3bbcb414e537a8c07cd7a8d82c63
zielisgabriel <josegabrielas0586@gmail.com>
2025-06-14 17:14:10 -0300

    feat: New Haircut Services and some modifications

---
+++ src/main/java/br/com/gabriel/barbershop_appointment_api/services/DeleteHairCutService.java
@@ -0,0 +1,27 @@
+package br.com.gabriel.barbershop_appointment_api.services;
+
+import java.util.Optional;
+import java.util.UUID;
+
+import org.springframework.stereotype.Service;
+
+import br.com.gabriel.barbershop_appointment_api.exceptions.HairCutNotFoundException;
+import br.com.gabriel.barbershop_appointment_api.models.HairCut;
+import br.com.gabriel.barbershop_appointment_api.repositories.HairCutRepository;
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+public class DeleteHairCutService {
+    private final HairCutRepository hairCutRepository;
+
+    public void execute(UUID haircutId) {
+        Optional<HairCut> hairCut = hairCutRepository.findByHairCutId(haircutId);
+
+        if (!hairCut.isPresent()) {
+            throw new HairCutNotFoundException();
+        }
+
+        this.hairCutRepository.deleteById(haircutId);
+    }
+}
