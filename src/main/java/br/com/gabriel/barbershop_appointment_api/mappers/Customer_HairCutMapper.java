a52bd039f7fd1f1684ba7b384beab3955a27ff22
zielisgabriel <josegabrielas0586@email.com>
2025-06-19 23:06:51 -0300

    feat: Customer_HairCut Mapper added

---
+++ src/main/java/br/com/gabriel/barbershop_appointment_api/mappers/Customer_HairCutMapper.java
@@ -0,0 +1,24 @@
+package br.com.gabriel.barbershop_appointment_api.mappers;
+
+import java.time.LocalDateTime;
+
+import org.springframework.stereotype.Component;
+
+import br.com.gabriel.barbershop_appointment_api.models.Customer;
+import br.com.gabriel.barbershop_appointment_api.models.Customer_HairCut;
+import br.com.gabriel.barbershop_appointment_api.models.HairCut;
+import lombok.RequiredArgsConstructor;
+
+@Component
+@RequiredArgsConstructor
+public class Customer_HairCutMapper {
+    public Customer_HairCut map(Customer customer, HairCut hairCut, LocalDateTime appointmentDateTime) {
+        Customer_HairCut customer_HairCut = new Customer_HairCut();
+
+        customer_HairCut.setCustomer(customer);
+        customer_HairCut.setHaircut(hairCut);
+        customer_HairCut.setAppointmentDateTime(appointmentDateTime);
+        
+        return customer_HairCut;
+    }
+}
