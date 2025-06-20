633d85d6eb7931ab6a9170cde16491afc686720a
zielisgabriel <josegabrielas0586@email.com>
2025-06-19 23:11:29 -0300

    feat: Available Time Respository added

---
+++ src/main/java/br/com/gabriel/barbershop_appointment_api/repositories/AvailableTimeRepository.java
@@ -0,0 +1,12 @@
+package br.com.gabriel.barbershop_appointment_api.repositories;
+
+import java.time.LocalTime;
+import java.util.UUID;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import br.com.gabriel.barbershop_appointment_api.models.AvailableTime;
+
+public interface AvailableTimeRepository extends JpaRepository<AvailableTime, UUID> {
+    boolean existsByTime(LocalTime time);
+}
