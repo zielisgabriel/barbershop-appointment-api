23eba4fb7245efa01a0a54700b6ab30ed812c182
zielisgabriel <josegabrielas0586@email.com>
2025-06-19 23:12:52 -0300

    feat: Methods added for fetch Customer_Haircut in database

--- src/main/java/br/com/gabriel/barbershop_appointment_api/repositories/Customer_HairCutRepository.java
+++ src/main/java/br/com/gabriel/barbershop_appointment_api/repositories/Customer_HairCutRepository.java
@@ -1,9 +1,16 @@
 package br.com.gabriel.barbershop_appointment_api.repositories;
 
+import java.time.LocalDateTime;
+import java.util.Optional;
 import java.util.UUID;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import br.com.gabriel.barbershop_appointment_api.models.Customer;
 import br.com.gabriel.barbershop_appointment_api.models.Customer_HairCut;
 
-public interface Customer_HairCutRepository extends JpaRepository<Customer_HairCut, UUID> {}
+public interface Customer_HairCutRepository extends JpaRepository<Customer_HairCut, UUID> {
+    Optional<Customer_HairCut> findByCustomerHaircutId(UUID customerHaircutId);
+    Optional<Customer_HairCut> findByCustomer(Customer customer);
+    Optional<Customer_HairCut> findByAppointmentDateTime(LocalDateTime appointmentDateTime);
+}
