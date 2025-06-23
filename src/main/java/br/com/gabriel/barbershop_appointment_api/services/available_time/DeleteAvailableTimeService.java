40c2664f9aeb3db612b47644687b182c099328a0
zielisgabriel <josegabrielas0586@email.com>
2025-06-23 12:15:55 -0300

    feat: Delete Availablee Time Service added

---
+++ src/main/java/br/com/gabriel/barbershop_appointment_api/services/available_time/DeleteAvailableTimeService.java
@@ -0,0 +1,23 @@
+package br.com.gabriel.barbershop_appointment_api.services.available_time;
+
+import java.util.UUID;
+
+import org.springframework.stereotype.Service;
+
+import br.com.gabriel.barbershop_appointment_api.exceptions.AvailableTimeNotFoundException;
+import br.com.gabriel.barbershop_appointment_api.models.AvailableTime;
+import br.com.gabriel.barbershop_appointment_api.repositories.AvailableTimeRepository;
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+public class DeleteAvailableTimeService {
+    private final AvailableTimeRepository availableTimeRepository;
+
+    public void execute(UUID availableTimeId) {
+        AvailableTime availableTime = this.availableTimeRepository.findByAvailableTimeId(availableTimeId)
+            .orElseThrow(() -> new AvailableTimeNotFoundException());
+
+        this.availableTimeRepository.delete(availableTime);
+    }
+}
