75d0eca513b14547c2d17c7dd614577d64dde698
zielisgabriel <josegabrielas0586@email.com>
2025-06-24 09:42:57 -0300

    remove: Password check removed from default profile

--- src/main/java/br/com/gabriel/barbershop_appointment_api/config/BarbershopAppointmentAuthenticationProvider.java
+++ src/main/java/br/com/gabriel/barbershop_appointment_api/config/BarbershopAppointmentAuthenticationProvider.java
@@ -2,13 +2,11 @@ package br.com.gabriel.barbershop_appointment_api.config;
 
 import org.springframework.context.annotation.Profile;
 import org.springframework.security.authentication.AuthenticationProvider;
-import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
 
 import lombok.RequiredArgsConstructor;
@@ -18,7 +16,6 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public class BarbershopAppointmentAuthenticationProvider implements AuthenticationProvider {
     private final UserDetailsService userDetailsService;
-    private final PasswordEncoder passwordEncoder;
 
 	@Override
 	public Authentication authenticate(Authentication authentication) throws AuthenticationException {
@@ -27,16 +24,12 @@ public class BarbershopAppointmentAuthenticationProvider implements Authenticati
 
         UserDetails user = userDetailsService.loadUserByUsername(username);
 
-        if (passwordEncoder.matches(password, user.getPassword())) {
-            return new UsernamePasswordAuthenticationToken(
-                username,
-                password,
-                user.getAuthorities()
-            );
-        } else {
-            throw new BadCredentialsException("Invalid password");
-        }
-	}
+        return new UsernamePasswordAuthenticationToken(
+            username,
+            password,
+            user.getAuthorities()
+        );
+    }
 
 	@Override
 	public boolean supports(Class<?> authentication) {
