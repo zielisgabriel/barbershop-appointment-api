592df06e763b2678001a2d3a1a1001463f3b3cb9
zielisgabriel <josegabrielas0586@email.com>
2025-06-30 18:02:12 -0300

    refactor: Modify error handling sintax

--- src/main/java/br/com/gabriel/barbershop_appointment_api/exceptions/BarbershopAuthenticationEntryPointHandler.java
+++ src/main/java/br/com/gabriel/barbershop_appointment_api/exceptions/BarbershopAuthenticationEntryPointHandler.java
@@ -1,6 +1,7 @@
 package br.com.gabriel.barbershop_appointment_api.exceptions;
 
 import java.io.IOException;
+import java.time.LocalDateTime;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.security.core.AuthenticationException;
@@ -13,7 +14,22 @@ import jakarta.servlet.http.HttpServletResponse;
 public class BarbershopAuthenticationEntryPointHandler implements AuthenticationEntryPoint {
 	@Override
 	public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException authException) throws IOException, ServletException {
+		LocalDateTime currentDateTime = LocalDateTime.now();
+		String message = "Não autorizado!";
+		String path = request.getRequestURI();
+		
 		response.setHeader("barbershop-error-reason", "Authentication Failed");
-		response.sendError(HttpStatus.UNAUTHORIZED.value(), "Não autorizado.");
+		response.setStatus(HttpStatus.UNAUTHORIZED.value());
+		response.setContentType("application/json;charset=UTF8");
+
+		String responseJson = String.format("{\"timestamp\": \"%s\", \"status\": %d, \"error\": \"%s\", \"message\": \"%s\", \"path\": \"%s\"}", 
+			currentDateTime,
+			HttpStatus.UNAUTHORIZED.value(),
+			HttpStatus.UNAUTHORIZED.getReasonPhrase(),
+			message,
+			path
+		);
+
+		response.getWriter().write(responseJson);
     }
 }
