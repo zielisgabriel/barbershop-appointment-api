b48263a376e6310d28cf6061533bee1793e591e8
zielisgabriel <josegabrielas0586@email.com>
2025-07-10 09:57:32 -0300

    feat: Scheduling enabled

--- src/main/java/br/com/gabriel/barbershop_appointment_api/BarbershopAppointmentApiApplication.java
+++ src/main/java/br/com/gabriel/barbershop_appointment_api/BarbershopAppointmentApiApplication.java
@@ -2,8 +2,10 @@ package br.com.gabriel.barbershop_appointment_api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class BarbershopAppointmentApiApplication {
 
 	public static void main(String[] args) {
